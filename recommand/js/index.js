7cc4bbc912bc43b24eff6d169fcb976973b4ac07
ProjectGroup01 <127112870+ProjectGroup01@users.noreply.github.com>
2023-04-04 17:22:08 +0900

    추가끝

--- recommand/js/index.js
+++ recommand/js/index.js
@@ -22,6 +22,89 @@ $(function(){
           
      });
 
+// sec1 부분 
+      
+const elts = {
+  text1: document.getElementById("text1"),
+  text2: document.getElementById("text2")
+};
+
+const texts = [
+  "추억을 담고 싶은 곳이 안 떠오르시나요?",
+  "아래의 추천 캠핑장을 확인해보세요!"
+];
+
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+  morph -= cooldown;
+  cooldown = 0;
+
+  let fraction = morph / morphTime;
+
+  if (fraction > 1) {
+      cooldown = cooldownTime;
+      fraction = 1;
+  }
+
+  setMorph(fraction);
+}
+
+function setMorph(fraction) {
+  elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  fraction = 1 - fraction;
+  elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+  elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+  elts.text1.textContent = texts[textIndex % texts.length];
+  elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+  morph = 0;
+
+  elts.text2.style.filter = "";
+  elts.text2.style.opacity = "100%";
+
+  elts.text1.style.filter = "";
+  elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  let newTime = new Date();
+  let shouldIncrementIndex = cooldown > 0;
+  let dt = (newTime - time) / 3000;
+  time = newTime;
+
+  cooldown -= dt;
+
+  if (cooldown <= 0) {
+      if (shouldIncrementIndex) {
+          textIndex++;
+      }
+
+      doMorph();
+  } else {
+      doCooldown();
+  }
+}
+
+animate();
+
+
      $('.slide-group').slick({
         autoplay:true,
         autoplaySpeed:3000,
@@ -32,11 +115,39 @@ $(function(){
         nextArrow:'<button class="slick-arrow slick-next"><i class="fa-solid fa-angles-right"></i></button>',
     
     })
-    $(".slideright").on("click", function() {
-        $(".detailwrap").addClass("on");
+    $(".slide1 .slideright").on("click", function() {
+        $(".wrap1").addClass("on");
       });
-      $(".detailwrap i").on("click", function() {
-        $(".detailwrap").removeClass("on");
+      $(".wrap1 i").on("click", function() {
+        $(".wrap1").removeClass("on");
+      });
+
+      $(".slide2 .slideright").on("click", function() {
+        $(".wrap2").addClass("on");
+      });
+      $(".wrap2 i").on("click", function() {
+        $(".wrap2").removeClass("on");
+      });
+
+      $(".slide3 .slideright").on("click", function() {
+        $(".wrap3").addClass("on");
+      });
+      $(".wrap3 i").on("click", function() {
+        $(".wrap3").removeClass("on");
+      });
+
+      $(".slide4 .slideright").on("click", function() {
+        $(".wrap4").addClass("on");
+      });
+      $(".wrap4 i").on("click", function() {
+        $(".wrap4").removeClass("on");
+      });
+
+      $(".slide5 .slideright").on("click", function() {
+        $(".wrap5").addClass("on");
+      });
+      $(".wrap5 i").on("click", function() {
+        $(".wrap5").removeClass("on");
       });
        // 헤더 scroll
   $(window).scroll(function () {
